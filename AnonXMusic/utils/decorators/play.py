4ea053c5a43136f7700c1f97f7e8bd114a3d6c01
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-07 15:55:49 +0300

    Update play.py

--- AnonXMusic/utils/decorators/play.py
+++ AnonXMusic/utils/decorators/play.py
@@ -87,7 +87,7 @@ def PlayWrapper(command):
                 else:
                     if message.from_user.id not in admins:
                         return await message.reply_text(_["play_4"])
-        if message.command[0][0] == "v":
+        if message.command[0][0] == "v" or message.command[0][0] == "ڤ":
             video = True
         else:
             if "-v" in message.text:
